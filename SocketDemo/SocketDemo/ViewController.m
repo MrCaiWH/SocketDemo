6681c054c4312bf46af620edc7ac4a6b14de2b0c
huanghuacai <18811108252@163.com>
2015-11-16 21:40:23 +0800

    轻微调整

--- SocketDemo/SocketDemo/ViewController.m
+++ SocketDemo/SocketDemo/ViewController.m
@@ -7,12 +7,9 @@
 //
 
 #import "ViewController.h"
-#import "AsyncSocket.h"
 
-@interface ViewController ()<AsyncSocketDelegate>
-{
-    AsyncSocket *socket;
-}
+@interface ViewController ()
+
 @end
 
 @implementation ViewController
@@ -20,28 +17,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
 
-    socket=[[AsyncSocket alloc] initWithDelegate:self];
-    [socket connectToHost:@"www.baidu.com" onPort:80 error:nil];
     
-    [socket readDataToLength:50 withTimeout:5 tag:1];
-    [socket readDataToLength:50 withTimeout:5 tag:2];
-    [socket writeData:[@"GET / HTTP/1.1\n\n" dataUsingEncoding:NSUTF8StringEncoding] withTimeout:3 tag:1];
-
-    
-}
-
-- (void)onSocket:(AsyncSocket *)sock didConnectToHost:(NSString *)host port:(UInt16)port{
-    NSLog(@"did connect to host");
-}
-
-- (void)onSocket:(AsyncSocket *)sock didReadData:(NSData *)data withTag:(long)tag{
-    NSLog(@"did read data");
-    NSString* message = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
-    NSLog(@"message is: \n%@",message);
-}
-
-- (void)onSocketDidDisconnect:(AsyncSocket *)sock{
-    NSLog(@"socket did disconnect");
 }
 
 @end
